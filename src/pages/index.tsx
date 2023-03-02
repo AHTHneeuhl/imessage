d69d7bae98d5b65d1bb1921ebb5d0e20207a5264
Shubham Patrick <ahthneeuhl@gmail.com>
2023-03-02 23:33:40 +0530

    NextAuth Google Provider SignIn & SignOut

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return null;
-}
+import { NextPage } from "next";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+const Home: NextPage = () => {
+  const { data } = useSession();
+
+  return (
+    <div>
+      {data?.user ? (
+        <button onClick={() => signOut()}>Sign Out</button>
+      ) : (
+        <button onClick={() => signIn("google")}>Sign In</button>
+      )}
+      {data?.user?.name}
+    </div>
+  );
+};
+
+export default Home;
