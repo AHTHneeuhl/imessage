668adfaa3f400b4d1fe6c9b31785ce0313a5a2fd
Shubham Patrick <ahthneeuhl@gmail.com>
2023-06-29 22:38:24 +0530

    chore: Create types for Messages and Conversation

--- backend/src/utils/types.ts
+++ backend/src/utils/types.ts
@@ -1,19 +1,36 @@
 import { ISODateString } from "next-auth";
 import { Prisma, PrismaClient } from "@prisma/client";
+import { PubSub } from "graphql-subscriptions";
+import { Context } from "graphql-ws/lib/server";
+import {
+  conversationPopulated,
+  participantPopulated,
+} from "../graphql/resolvers/conversation";
+import { messagePopulated } from "../graphql/resolvers/messages";
 
-export interface GraphQLContext {
-  session: Session | null;
-  prisma: PrismaClient;
-  // pubsub
-}
-
-// Users
-
+/**
+ * Server Configuration
+ */
 export interface Session {
   user?: User;
   expires: ISODateString;
 }
 
+export interface GraphQLContext {
+  session: Session | null;
+  prisma: PrismaClient;
+  pubsub: PubSub;
+}
+
+export interface SubscriptionContext extends Context {
+  connectionParams: {
+    session?: Session;
+  };
+}
+
+/**
+ * User
+ */
 export interface User {
   id: string;
   username?: string;
@@ -28,6 +45,10 @@ export interface CreateUsernameResponse {
   error?: string;
 }
 
+export interface SearchUsersResponse {
+  users: Array<User>;
+}
+
 /**
  * Conversation
  */
@@ -37,4 +58,38 @@ export type ConversationPopulated = Prisma.ConversationGetPayload<{
 
 export type ParticipantPopulated = Prisma.ConversationParticipantGetPayload<{
   include: typeof participantPopulated;
+}>;
+
+export interface ConversationCreatedSubscriptionPayload {
+  conversationCreated: ConversationPopulated;
+}
+
+export interface ConversationUpdatedSubscriptionData {
+  conversationUpdated: {
+    conversation: ConversationPopulated;
+    addedUserIds: Array<string>;
+    removedUserIds: Array<string>;
+  };
+}
+
+export interface ConversationDeletedSubscriptionPayload {
+  conversationDeleted: ConversationPopulated;
+}
+
+/**
+ * Messages
+ */
+export interface SendMessageArguments {
+  id: string;
+  conversationId: string;
+  senderId: string;
+  body: string;
+}
+
+export interface SendMessageSubscriptionPayload {
+  messageSent: MessagePopulated;
+}
+
+export type MessagePopulated = Prisma.MessageGetPayload<{
+  include: typeof messagePopulated;
 }>;
