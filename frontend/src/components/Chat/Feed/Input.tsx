922540bc40fc357f1f33584593f843ce2d0f4d3c
Shubham Patrick <ahthneeuhl@gmail.com>
2023-06-30 12:22:16 +0530

    feat: Create Input, Messages, Header & MessageItem components

---
+++ frontend/src/components/Chat/Feed/Input.tsx
@@ -0,0 +1,109 @@
+import MessageOperations from "@/graphql/operations/message";
+import { MessagesData, SendMessageVariables } from "@/utils/types";
+import { useMutation } from "@apollo/client";
+import { Box, Input } from "@chakra-ui/react";
+import { ObjectId } from "bson";
+import { Session } from "next-auth";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+
+interface MessageInputProps {
+  session: Session;
+  conversationId: string;
+}
+
+const MessageInput: React.FC<MessageInputProps> = ({
+  session,
+  conversationId,
+}) => {
+  const [messageBody, setMessageBody] = useState("");
+
+  const [sendMessage] = useMutation<
+    { sendMessage: boolean },
+    SendMessageVariables
+  >(MessageOperations.Mutations.sendMessage);
+
+  const onSendMessage = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    try {
+      const { id: senderId } = session.user;
+      const newId = new ObjectId().toString();
+      const newMessage: SendMessageVariables = {
+        id: newId,
+        senderId,
+        conversationId,
+        body: messageBody,
+      };
+      const { data, errors } = await sendMessage({
+        variables: {
+          ...newMessage,
+        },
+        optimisticResponse: {
+          sendMessage: true,
+        },
+        update: (cache) => {
+          setMessageBody("");
+          const existing = cache.readQuery<MessagesData>({
+            query: MessageOperations.Query.messages,
+            variables: { conversationId },
+          }) as MessagesData;
+
+          cache.writeQuery<MessagesData, { conversationId: string }>({
+            query: MessageOperations.Query.messages,
+            variables: { conversationId },
+            data: {
+              ...existing,
+              messages: [
+                {
+                  id: newId,
+                  body: messageBody,
+                  senderId: session.user.id,
+                  conversationId,
+                  sender: {
+                    id: session.user.id,
+                    username: session.user.username,
+                  },
+                  createdAt: new Date(Date.now()),
+                  updatedAt: new Date(Date.now()),
+                },
+                ...existing.messages,
+              ],
+            },
+          });
+        },
+      });
+
+      if (!data?.sendMessage || errors) {
+        throw new Error("Error sending message");
+      }
+    } catch (error: any) {
+      console.log("onSendMessage error", error);
+      toast.error(error?.message);
+    }
+  };
+
+  return (
+    <Box px={4} py={6} width="100%">
+      <form onSubmit={onSendMessage}>
+        <Input
+          value={messageBody}
+          onChange={(event) => setMessageBody(event.target.value)}
+          size="md"
+          placeholder="New message"
+          color="whiteAlpha.900"
+          resize="none"
+          _focus={{
+            boxShadow: "none",
+            border: "1px solid",
+            borderColor: "whiteAlpha.300",
+          }}
+          _hover={{
+            borderColor: "whiteAlpha.300",
+          }}
+        />
+      </form>
+    </Box>
+  );
+};
+export default MessageInput;
---
+++ frontend/src/components/Chat/Feed/Messages/Messages.tsx
@@ -0,0 +1,91 @@
+import { useQuery } from "@apollo/client";
+import { Box, Flex, Stack } from "@chakra-ui/react";
+import React, { useEffect, useRef } from "react";
+import toast from "react-hot-toast";
+import MessageOperations from "@/graphql/operations/message";
+import {
+  MessagesData,
+  MessagesSubscriptionData,
+  MessagesVariables,
+} from "@/utils/types";
+import SkeletonLoader from "@/components/common/SkeletonLoader";
+import MessageItem from "./MessageItem";
+
+interface MessagesProps {
+  userId: string;
+  conversationId: string;
+}
+
+const Messages: React.FC<MessagesProps> = ({ userId, conversationId }) => {
+  const { data, loading, error, subscribeToMore } = useQuery<
+    MessagesData,
+    MessagesVariables
+  >(MessageOperations.Query.messages, {
+    variables: {
+      conversationId,
+    },
+    onError: ({ message }) => {
+      toast.error(message);
+    },
+  });
+
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  const subscribeToMoreMessages = (conversationId: string) => {
+    return subscribeToMore({
+      document: MessageOperations.Subscriptions.messageSent,
+      variables: {
+        conversationId,
+      },
+      updateQuery: (prev, { subscriptionData }: MessagesSubscriptionData) => {
+        if (!subscriptionData.data) return prev;
+
+        const newMessage = subscriptionData.data.messageSent;
+
+        return Object.assign({}, prev, {
+          messages:
+            newMessage.sender.id === userId
+              ? prev.messages
+              : [newMessage, ...prev.messages],
+        });
+      },
+    });
+  };
+
+  useEffect(() => {
+    const unsubscribe = subscribeToMoreMessages(conversationId);
+
+    return () => unsubscribe();
+  }, [conversationId]);
+
+  useEffect(() => {
+    if (!messagesEndRef.current || !data) return;
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  }, [data, messagesEndRef.current]);
+
+  if (error) {
+    return null;
+  }
+
+  return (
+    <Flex direction="column" justify="flex-end" overflow="hidden">
+      {loading && (
+        <Stack spacing={4} px={4}>
+          <SkeletonLoader count={4} height="60px" width="100%" />
+        </Stack>
+      )}
+      {data?.messages && (
+        <Flex direction="column-reverse" overflowY="scroll" height="100%">
+          {data.messages.map((message) => (
+            <MessageItem
+              key={message.id}
+              message={message}
+              sentByMe={message.sender.id === userId}
+            />
+          ))}
+        </Flex>
+      )}
+    </Flex>
+  );
+};
+export default Messages;
