0da6d609efd5beb552720ff47fb6477ca24cf1ea
Shubham Patrick <ahthneeuhl@gmail.com>
2023-07-03 13:16:27 +0530

    feat: Redirect to home page based on session & conversationId

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,18 +3,27 @@ import { Chat } from "@/components/Chat";
 import { Box } from "@chakra-ui/react";
 import { NextPage, NextPageContext } from "next";
 import { getSession, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
+  const router = useRouter();
 
   const reloadSession = () => {
     const event = new Event("visibilitychange");
     document.dispatchEvent(event);
   };
 
+  useEffect(() => {
+    if (!session?.user && router.query.conversationId) {
+      router.replace(process.env.NEXT_PUBLIC_BASE_URL as string);
+    }
+  }, [session?.user, router.query, router]);
+
   return (
     <Box>
-      {session?.user.username ? (
+      {session && session?.user?.username ? (
         <Chat session={session} />
       ) : (
         <Auth session={session} reloadSession={reloadSession} />
